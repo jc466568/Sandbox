69e72c2d701db10d93b24003bdc8a0a0b7b5076d
jc466568 <elwyn.omanga@my.jcu.edu.au>
2018-08-16 13:46:04 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,11 @@
 
 """ Elwyn Omanga """
+password = 0
+try:
+    password = input("Enter a password at least 10 characters long: ")
+except len(password) < 1:
+    password = input("Enter a password at least 10 characters long: ")
+while len(password) < 10:
+    password = input("Enter a password at least 10 characters long: ")
+print('*'*len(password))
 
